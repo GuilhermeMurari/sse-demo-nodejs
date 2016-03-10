8f2ef72a1a5cf0064128470a11dd33ff7cad253c
Guilherme Murari <guilherme.murari@dextra-sw.com>
2016-03-10 09:08:05 -0300

    server sent event poc

--- sse-demo-nodejs/server.js
+++ sse-demo-nodejs/server.js
@@ -1,14 +1,42 @@
-var SSE = require('sse')
-  , http = require('http');
- 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('okay');
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+var engine = {
+    "port" : 9090
+};
+
+var clients = {};
+var lastKey = 0;
+
+app.get('/events', function(req, res) {
+	req.socket.setTimeout(99999999);
+
+	res.writeHead(200, {
+		'Content-Type': 'text/event-stream',
+		'Cache-Control': 'no-cache',
+		'Connection': 'keep-alive'
+	});
+	res.write('\n');
+
+	(function(lastKey) {
+		clients[lastKey] = res; // <- Add this client to those we consider "attached"
+		req.on("close", function(){delete clients[lastKey]}); // <- Remove this client when he disconnects
+	})(++lastKey)
 });
- 
-server.listen(8080, '127.0.0.1', function() {
-  var sse = new SSE(server);
-  sse.on('connection', function(client) {
-    client.send('hi there!');
-  });
+
+app.get('/store', function(req, res) {
+	console.log(clients.length + "\n");
+	for (var key in clients) {
+		var data = "data goes here";
+		clients[key].write("data: " + data + "\n\n");
+		console.log("client with id " + key + " notified");
+	}
+});
+
+var server = app.listen(engine.port, function() {
+    console.log('Listening on port %d', server.address().port);
 });
